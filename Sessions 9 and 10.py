3a7b1ac8f07e2b46d58e86f3b2e6138e7adeeb5c
matilderibeiro <matilde.csribeiro@gmail.com>
2025-02-07 11:27:08 +0100

    File Example

---
+++ Sessions 9 and 10.py
@@ -0,0 +1,7 @@
+# !! FILES !!
+
+# Files are a way to store information and prevent it from being lost.
+# It allows us to save something and get it back the moment we reset the computer.
+# The main actions we can perform with files are read and write.
+# We can write a life and/or we can read from a file.
+
